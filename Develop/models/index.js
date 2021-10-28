2ea9bfb9c1772bb8ef9d9f4519fef1919697752a
Jack Carpenter <jackcarpenter1@live.com>
2021-10-27 19:29:48 -0500

    Setting up relationships

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,40 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'ProducttoCategory'
+});
 
 // Categories have many Products
+Category.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'CategorytoProduct'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'ProducttoTag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'TagtoProduct'
+})
 
 module.exports = {
   Product,
